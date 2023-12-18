dde87e7572101f11af136cd8420c91109ff81057
Maria Fomina <99403468+IsaBarling@users.noreply.github.com>
2023-12-18 21:19:38 +0300

    models.py

--- django-medical-app-master/pages/models.py
+++ django-medical-app-master/pages/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.IntegerField()
+    
+    class Meta:
+        app_label = 'pages'
 
     def __str__(self):
         return self.user.username
