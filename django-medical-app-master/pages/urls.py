fa3913eaf93200109e5c2e47fc528fa7602967ea
Maria Fomina <99403468+IsaBarling@users.noreply.github.com>
2023-12-19 16:23:41 +0300

    urls.py

--- django-medical-app-master/pages/urls.py
+++ django-medical-app-master/pages/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('stat/', StatPageView.as_view(), name='stat_page'),
     path('report/', MedicalRecordCreateView.as_view(), name='patient_home'),
-    path('dashboard/', show_doc_info, name='practitioner_home'),
     path('accounts/login/', PatientLoginView.as_view(), name='patient_login'),
     path('accounts/signup/', PatientSignUpView.as_view(), name='patient_signup'),
     path('accounts/p/login/', DoctorLoginView.as_view(), name='practitioner_login'),
