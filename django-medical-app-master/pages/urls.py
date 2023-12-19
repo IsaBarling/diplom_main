d937d12b3b3c703ca8da35909205ecc584f53af8
Maria Fomina <99403468+IsaBarling@users.noreply.github.com>
2023-12-19 16:15:14 +0300

    urls.py

--- django-medical-app-master/pages/urls.py
+++ django-medical-app-master/pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from pages.views.general import StatPageView, HomePageView
 from pages.views.patients import PatientSignUpView, PatientLoginView, MedicalRecordCreateView
 from pages.views.practitioner import (
-    DoctorLoginView as practitioner_login_page,
-    DoctorLogoutView as practitioner_logout_page,
-    signup_page as practitioner_signup_page,
+    DoctorLoginView,
+    DoctorLogoutView,
+    DoctorSignupView,
     show_doc_info
 )
 
@@ -17,6 +17,6 @@ urlpatterns = [
     path('dashboard/', show_doc_info, name='practitioner_home'),
     path('accounts/login/', PatientLoginView.as_view(), name='patient_login'),
     path('accounts/signup/', PatientSignUpView.as_view(), name='patient_signup'),
-    path('accounts/p/logout/', practitioner_logout_page, name='practitioner_logout'),
+    path('accounts/p/logout/', DoctorLogoutView.as_view(), name='practitioner_logout'),
 ]
 
